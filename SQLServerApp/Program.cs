de12dbd957f948eb7006ad59fd24c53d5c7b2f30
hirahira <th11282@gmail.com>
2024-02-13 20:45:59 +0900

    SQLServerAppの更新

--- SQLServerApp/Program.cs
+++ SQLServerApp/Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+
+// 先ほどコピーした接続文字列を貼り付ける
+var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=testdb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+// 実行するSQL
+var sql = "SELECT * FROM Test";
+
+// 接続のためのオブジェクトを生成
+// 実行後にオブジェクトのCloseが必要なためusing文で囲う
+using (var connection = new SqlConnection(connectionString))
+{
+    // 接続を確立
+    connection.Open();
+
+    // SqlCommand：DBにSQL文を送信するためのオブジェクトを生成
+    // SqlDataReader：読み取ったデータを格納するためのオブジェクトを生成
+    using (var command = new SqlCommand(sql, connection))
+    using (var reader = command.ExecuteReader())
+    {
+        // 1行ごとに読み取る
+        while (reader.Read())
+        {
+            // 列名を指定して、読み取ったデータをコンソール上に表示
+            Console.WriteLine($"Name：{reader["Name"]}   Age：{reader["Age"]}   Birthday：{reader["Birthday"]}");
+        }
+    }
+}
